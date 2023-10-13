a16ab667dbf09c86c0d592e101cbcc13d6b2d466
Fernando Reyes <fernandoreyes.dev@gmail.com>
2023-10-13 15:20:42 -0700

    Added metadata to specific components
    Signed-off-by: Fernando Reyes <fernandoreyes.dev@gmail.com>

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -9,11 +9,16 @@
 </template>
 
 <script setup>
+const { data: products } = await useFetch("https://fakestoreapi.com/products");
+
 definePageMeta({
   layout: "products",
 });
 
-const { data: products } = await useFetch("https://fakestoreapi.com/products");
+useHead({
+  title: "Nuxt Dojo | Merch",
+  meta: [{ name: "description", content: "Nuxt 3 Merch" }],
+});
 </script>
 
 <style scoped></style>
